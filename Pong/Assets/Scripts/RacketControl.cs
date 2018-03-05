aea41158e81107bc306617b8d1a6651aca6a21d5
Sakib <skb.noshtocod3r@gmail.com>
2018-03-05 11:48:02 +0600

    Added racket movement logic
    #Input Manager - new input 'Vertical2'
    positive, negative button set

---
+++ Pong/Assets/Scripts/RacketControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketControl : MonoBehaviour {
+
+	public float speed = 30f;
+	public string axis = "Vertical";
+	void FixedUpdate()
+	{
+		float v = Input.GetAxisRaw(axis);
+		GetComponent<Rigidbody2D>().velocity = Vector2.up * v * speed;
+	}
+}
