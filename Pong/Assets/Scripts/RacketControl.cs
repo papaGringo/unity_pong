11116e5a0c45dbe56515b59d80eb0178b138d22c
Sakib <skb.noshtocod3r@gmail.com>
2018-03-05 16:35:01 +0600

    Added a simple non-beatable AI

--- Pong/Assets/Scripts/RacketControl.cs
+++ Pong/Assets/Scripts/RacketControl.cs
@@ -5,10 +5,43 @@ using UnityEngine;
 public class RacketControl : MonoBehaviour {
 
 	public float speed = 30f;
+	public bool isAIEnabled = false;
 	public string axis = "Vertical";
+
+	private GameObject ball;
+	void Update()
+	{
+		if(isAIEnabled)
+		{
+			ball = GameObject.FindGameObjectWithTag("ball");
+		}
+		else
+		{
+			ball = null;
+		}
+
+	}
 	void FixedUpdate()
 	{
-		float v = Input.GetAxisRaw(axis);
-		GetComponent<Rigidbody2D>().velocity = Vector2.up * v * speed;
+		if(!isAIEnabled)
+		{
+			float v = Input.GetAxisRaw(axis);
+			GetComponent<Rigidbody2D>().velocity = Vector2.up * v * speed;
+		}
+		else
+		{	
+			StartCoroutine("AIControl")	;
+		}
+	}
+
+	IEnumerator AIControl()
+	{
+		while(!ball)
+		{
+			yield return null;
+		}
+		float d = ball.transform.position.y - transform.position.y;
+		//Vector2 spdAI = new Vector2(0, d>0? Mathf.Min(d, 1.0f) : Mathf.Min(-d, 1.0f) );
+		GetComponent<Rigidbody2D>().velocity = Vector2.up * speed * d;
 	}
 }
