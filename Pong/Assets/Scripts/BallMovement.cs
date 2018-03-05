1aa274d5587025e002955b0cbd76387c0c8f3dd5
Sakib <skb.noshtocod3r@gmail.com>
2018-03-05 12:43:31 +0600

    Ball Movement Collision Handling with Racket

--- Pong/Assets/Scripts/BallMovement.cs
+++ Pong/Assets/Scripts/BallMovement.cs
@@ -10,4 +10,21 @@ public class BallMovement : MonoBehaviour {
 	{
 		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
 	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.tag == "racket")
+		{
+			float y = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.y);
+			Vector2 direction = new Vector2(collision.gameObject.name == "RacketHolderLeft"? 1: -1 , y).normalized;
+
+			GetComponent<Rigidbody2D>().velocity = direction * speed;
+		}
+	}
+
+	float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight)
+	{
+		//1 top of racket ; 0 middle of racket; -1 bottom of racket
+		return (ballPos.y - racketPos.y)/ racketHeight;
+	}
 }
