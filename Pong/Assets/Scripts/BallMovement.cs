f823847b5cd5812b2899dbe74c83663b5a7352ff
Sakib <skb.noshtocod3r@gmail.com>
2018-03-05 12:06:42 +0600

    Ball Movement Logic

---
+++ Pong/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+	public float speed = 30f;
+
+	void Start()
+	{
+		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+}
